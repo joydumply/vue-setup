8485938f03b293caf059c31f1179e833ec9399b6
joydumply <nick.babichev@gmail.com>
2023-11-26 22:04:26 +0200

    implemented v-for

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,51 @@
 	>
 		Link
 	</a>
+	<div>
+		<ul>
+			<li
+				v-for="(color, index) in colors"
+				:key="index"
+			>
+				{{ index }}
+				{{ color }}
+			</li>
+		</ul>
+
+		<div>
+			<input
+				type="text"
+				@keypress.enter="addNewColor"
+			/>
+		</div>
+
+		<hr />
+
+		<ul>
+			<li
+				v-for="user in users"
+				:key="user.id"
+			>
+				{{ user.name + ': ' + user.age }}
+			</li>
+		</ul>
+
+		<hr />
+
+		<ul>
+			<li
+				v-for="(value, name, index) in product"
+				:key="index"
+			>
+				{{ name }}: {{ value }}
+			</li>
+		</ul>
+
+		<input
+			type="text"
+			@keypress.enter="deleteProp"
+		/>
+	</div>
 </template>
 
 <script>
@@ -22,6 +67,16 @@ export default {
 	name: 'app',
 	data: () => ({
 		text: '',
+		colors: ['orange', 'black', 'yellow', 'blue'],
+		users: [
+			{ name: 'Nik', age: 27, id: 991 },
+			{ name: 'Anya', age: 25, id: 777 },
+		],
+		product: {
+			brand: 'Apple',
+			model: 'iPhone 12 Pro',
+			price: '$1000',
+		},
 	}),
 	methods: {
 		onClick(value, event) {
@@ -33,34 +88,16 @@ export default {
 		onEnterPress(e) {
 			this.text = e.target.value;
 		},
+		addNewColor(e) {
+			// this.colors.push(e.target.value); // в случае массивов лучше использовать функции массивов push/pop
+			this.$set(this.colors, this.colors.length, e.target.value); // используется в основном для объектов чтоб отслеживать внутренние изменения
+			e.target.value = '';
+		},
+		deleteProp(e) {
+			delete this.product[e.target.value]; // работает на vue 3; не работает на vue 2
+			console.log(this);
+			// this.$delete(this.product, e.target.value); // используется на vue 2 и не работает на vue 3
+		},
 	},
 };
 </script>
-<style scoped>
-header {
-	line-height: 1.5;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-}
-</style>
